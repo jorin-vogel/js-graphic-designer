e87388959f73f6ac16f574341df7c310bfba60fe
Jorin Vogel <jorin.vogel@gmail.com>
2014-12-02 18:47:35 +0100

    simplify eventEmiter

--- core/eventEmitter.js
+++ core/eventEmitter.js
@@ -3,41 +3,16 @@ module.exports = function(obj) {
     var events = {};
     var slice = [].slice;
 
-
-    // Infinite handlers support:
-    // obj.on = function(event) {
-    //     var handlers = slice.call(arguments, 1);
-    //     for (var i = 0, handler; (handler = handlers[i]); ++i) {
-    //         (events[event] = events[event] || []).push(handler);
-    //     }
-    //     return obj;
-    // };
-
     obj.on = function(event, handler) {
         (events[event] = events[event] || []).push(handler);
         return obj;
     };
 
 
-    obj.once = function(event, handler) {
-        return obj.on(event, function onceWrapper() {
-            handler.apply(null, arguments);
-            obj.off(event, onceWrapper);
-        });
-    };
-
-
     obj.emit = function(event) {
         var i, handler;
         var args = slice.call(arguments, 1);
-        var handlers = [];
-
-        // emit sub:events
-        var parts = event.split(':');
-        for (i = 1; i <= parts.length; i++) {
-            var subHandlers = events[parts.slice(0, i).join(':')];
-            if (subHandlers) handlers = handlers.concat(subHandlers);
-        }
+        var handlers = events[event];
 
         // NOTE: doesn't log names for once listeners
         // DEBUG
@@ -67,27 +42,6 @@ module.exports = function(obj) {
 
         return obj;
     };
-
-
-    // obj.stop = function(event, handler) {
-
-    //     if (event === "*") {
-    //         events = {};
-    //     } else if (handler) {
-    //         var arr = events[event];
-    //         for (var i = 0, cb; (cb = arr && arr[i]); ++i) {
-    //             if (cb === handler) {
-    //                 arr.splice(i, 1); i--;
-    //             }
-    //         }
-    //     } else {
-    //         event.replace(/\S+/g, function(name) {
-    //             events[name] = [];
-    //         });
-    //     }
-    //     return obj;
-    // };
-
 
 
     return obj;
